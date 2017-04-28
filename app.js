ea44c771c4c1673a3e5a3c03e9188ed328242b3f
Amanda Koster <amandakoster@gmail.com>
2017-04-28 16:47:38 -0700

    calculator works

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-$(document).ready(function() {
+'use strict';
+var gardening = 0;
+var yardwork = 0;
+var landscaping = 0;
+var total = gardening + yardwork + landscaping;
 
-  $('#calculate').click(function() {
+$('#g-25').on('change',function() {
+  gardening = $(this).val() * 25;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
+});
 
-    if ($('#input').val() != '' && $('#input').val() != null) {
+$('#y-45').on('change',function() {
+  yardwork = $(this).val() * 45;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
+});
 
-      $('#result').html('total value is::' + parseInt($('#input').val()) * 30);
+$('#l-65').on('change',function() {
+  landscaping = $(this).val() * 65;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
+});
 
-    }
+$('#g-25').keyup(function() {
+  gardening = $(this).val() * 25;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
+});
 
-    else
-        {
-      $('#result').html('Please enter some value');
-    }
-  });
+$('#y-45').keyup(function() {
+  yardwork = $(this).val() * 45;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
+});
+
+$('#l-65').keyup(function() {
+  landscaping = $(this).val() * 65;
+  total = gardening + yardwork + landscaping;
+  console.log(total);
+  $('#total').text(total);
 });
