d2b6248a9e97c9346e531bd987b6a517756d334e
Amanda Koster <amandakoster@gmail.com>
2017-05-03 19:53:42 -0400

    working on TOD css

--- app.js
+++ app.js
@@ -46,32 +46,31 @@ $('#l-65').keyup(function() {
   $('#total').text(total);
 });
 
-// TOD stylesheet
-// <script>
-// <!--
-// function getStylesheet() {
-//       var currentTime = new Date().getHours();
-//       if (0 <= currentTime&&currentTime < 5) {
-//        document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
-//       }
-//       if (5 <= currentTime&&currentTime < 11) {
-//        document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
-//       }
-//       if (11 <= currentTime&&currentTime < 16) {
-//        document.write("<link rel='stylesheet' href='day.css' type='text/css'>");
-//       }
-//       if (16 <= currentTime&&currentTime < 22) {
-//        document.write("<link rel='stylesheet' href='evening.css' type='text/css'>");
-//       }
-//       if (22 <= currentTime&&currentTime <= 24) {
-//        document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
-//       }
-// }
-//
-// getStylesheet();
+// TOD stylesheet jQuery:
+function getStylesheet() {
+  var currentTime = new Date().getHours();
+  if (0 <= currentTime && currentTime < 5) {
+    document.write('<link rel=\'stylesheet\' href=\'night.css\' type=\'text/css\'>');
+    console.log();
+  }
+      // if (5 <= currentTime&&currentTime < 11) {
+      //  document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
+      // }
+  if (11 <= currentTime && currentTime < 16) {
+    document.write('<link rel=\'stylesheet\' href=\'day.css\' type=\'text/css\'>');
+  }
+      // if (16 <= currentTime&&currentTime < 22) {
+      //  document.write("<link rel='stylesheet' href='evening.css' type='text/css'>");
+      // }
+  if (22 <= currentTime && currentTime <= 24) {
+    document.write('<link rel=\'stylesheet\' href=\'night.css\' type=\'text/css\'>');
+  }
+}
+
+getStylesheet();
 // -->
 // </script>
-//
+
 // <noscript><link href="main.css" rel="stylesheet"></noscript>
 
 // http://codepen.io/jamesbarnett/pen/kdDBL
