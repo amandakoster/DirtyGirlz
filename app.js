c71709548845aa3e151aca7c63977194c69f94e7
Amanda Koster <amandakoster@gmail.com>
2017-05-05 14:11:16 -0700

    cleaning up js

--- app.js
+++ app.js
@@ -3,63 +3,55 @@ var gardening = 0;
 var yardwork = 0;
 var landscaping = 0;
 var total = gardening + yardwork + landscaping;
-
-$('#g-25').on('change',function() {
+$('#g-25').on('change', function() {
   gardening = $(this).val() * 25;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
-$('#y-45').on('change',function() {
+$('#y-45').on('change', function() {
   yardwork = $(this).val() * 45;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
-$('#l-65').on('change',function() {
+$('#l-65').on('change', function() {
   landscaping = $(this).val() * 65;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
 $('#g-25').keyup(function() {
   gardening = $(this).val() * 25;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
 $('#y-45').keyup(function() {
   yardwork = $(this).val() * 45;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
 $('#l-65').keyup(function() {
   landscaping = $(this).val() * 65;
   total = gardening + yardwork + landscaping;
   console.log(total);
   $('#total').text(total);
 });
-
-$(document).ready(function(){
+$(document).ready(function() {
   var d = new Date();
   var n = d.getHours();
   if (n > 19 || n < 6)
-  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+	// If time is after 7PM or before 6AM, apply night theme to ‘body’
     document.body.className = 'night';
   else if (n > 16 && n < 19)
-  // If time is between 4PM – 7PM sunset theme to ‘body’
+	// If time is between 4PM – 7PM sunset theme to ‘body’
     document.body.className = 'sunset';
   else
-  // Else use ‘day’ theme
-  document.body.className = 'day';
+	// Else use ‘day’ theme
+		document.body.className = 'day';
 });
-
 // // TOD stylesheet jQuery:
 // $(document).ready(function() {
 //
@@ -84,7 +76,5 @@ $(document).ready(function(){
 //
 // // -->
 // </script>
-
 // <noscript><link href="main.css" rel="stylesheet"></noscript>
-
 // http://codepen.io/jamesbarnett/pen/kdDBL
