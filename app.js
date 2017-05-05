1f68cee75ab4eaf6d6ef827cc5cda256fce3eb71
Amanda Koster <amandakoster@gmail.com>
2017-05-05 19:06:28 -0300

    fixing css

--- app.js
+++ app.js
@@ -6,39 +6,38 @@ var total = gardening + yardwork + landscaping;
 $('#g-25').on('change', function() {
   gardening = $(this).val() * 25;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
+
 $('#y-45').on('change', function() {
   yardwork = $(this).val() * 45;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
+
 $('#l-65').on('change', function() {
   landscaping = $(this).val() * 65;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
+
 $('#g-25').keyup(function() {
   gardening = $(this).val() * 25;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
+
 $('#y-45').keyup(function() {
   yardwork = $(this).val() * 45;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
 $('#l-65').keyup(function() {
   landscaping = $(this).val() * 65;
   total = gardening + yardwork + landscaping;
-  console.log(total);
   $('#total').text(total);
 });
+
 $(document).ready(function() {
   var d = new Date();
   var n = d.getHours();
